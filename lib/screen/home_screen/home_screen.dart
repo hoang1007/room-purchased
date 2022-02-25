81ce033a8e97bf4267c821a673eefdf3156ee9a8
hoang1007 <hoangvuhuy1007@gmail.com>
2022-02-25 22:16:56 +0700

    optimize switching pages

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -1,34 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hah/screen/home_screen/components/item_view/item_view_widget.dart';
 import 'package:hah/screen/home_screen/components/item_input_field/itemlist_inputfield_widget.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final double _threshold = 5;
-  int _choosenScreenId = 0;
-
-  final _screens = [
-    const ItemListInputFieldPane(itemCount: 10),
-    const ItemViewWidget()
-  ];
-
-  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onPanUpdate: (DragUpdateDetails details) {
-        if (details.delta.dx > _threshold || details.delta.dx < -_threshold) {
-          setState(() {
-            _choosenScreenId = (_choosenScreenId + 1) % _screens.length;
-          });
-        }
-      },
-      child: _screens[_choosenScreenId],
+    final pages = [
+      const ItemListInputFieldPane(itemCount: 10,),
+      const ItemViewWidget()];
+
+    return PageView(
+      controller: PageController(initialPage: pages.length),
+      children: pages,
     );
   }
 }
+//
+// class _HomeScreenState extends State<HomeScreen> {
+//   final double _threshold = 5;
+//   int _choosenScreenId = 0;
+//
+//   final _screens = [
+//     const ItemListInputFieldPane(itemCount: 10),
+//     const ItemViewWidget()
+//   ];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onPanUpdate: (DragUpdateDetails details) {
+//         if (details.delta.dx > _threshold || details.delta.dx < -_threshold) {
+//           setState(() {
+//             _choosenScreenId = (_choosenScreenId + 1) % _screens.length;
+//           });
+//         }
+//       },
+//       child: _screens[_choosenScreenId],
+//     );
+//   }
+// }
