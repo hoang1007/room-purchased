7f031971284c566dc2a3918069a4a169b56fa620
hoang1007 <hoangvuhuy1007@gmail.com>
2022-07-05 18:06:34 +0700

    change save button

--- lib/screen/home_screen/components/item_input_field/itemlist_inputfield_widget.dart
+++ lib/screen/home_screen/components/item_input_field/itemlist_inputfield_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hah/objects/item.dart';
-import 'package:hah/objects/monthtime.dart';
+import 'package:hah/objects/time.dart';
 import 'package:hah/objects/user_manager.dart';
 import 'item_inputfield_widget.dart';
 
@@ -14,98 +14,99 @@ class ItemListInputFieldPane extends StatelessWidget {
     var itemInputFieldList = ItemListInputField(length: itemCount);
 
     return Scaffold(
-        appBar: AppBar(),
         body: Form(
-            child: Column(children: [
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
           Expanded(child: itemInputFieldList),
           Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Unfocus the text field
-                  FocusManager.instance.primaryFocus?.unfocus();
+              padding: const EdgeInsets.only(bottom: 20, right: 20),
+              child: FloatingActionButton(
+                  onPressed: () {
+                    // Unfocus the text field
+                    FocusManager.instance.primaryFocus?.unfocus();
 
-                  final items = <Item>[];
+                    final items = <Item>[];
 
-                  try {
-                    for (int i = 0; i < itemCount; i++) {
-                      var item = itemInputFieldList[i].controller.getItem();
+                    try {
+                      for (int i = 0; i < itemCount; i++) {
+                        var item = itemInputFieldList[i].controller.getItem();
 
-                      if (item != null) {
-                        items.add(item);
-                      }
-                    }
-
-                    for (int i = 0; i < itemCount; i++) {
-                      itemInputFieldList[i].controller.clearTextField();
-                    }
-                  } on ArgumentError catch (e) {
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text(e.message)));
-
-                    return;
-                  }
-
-                  if (items.isNotEmpty) {
-                    var itemsInMonth = MapEntry(
-                        MonthTime.fromDateTime(items[0].purchasedTime), items);
-
-                    UserManager.instance.user.itemlist.addItemsInMonth(itemsInMonth);
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: FutureBuilder<void>(
-                      future: UserManager.instance.saveUser(),
-                      builder:
-                          (BuildContext context, AsyncSnapshot<void> snapshot) {
-                        List<Widget> children;
-
-                        if (snapshot.hasData) {
-                          children = <Widget>[
-                            const Icon(
-                              Icons.check_circle_outline,
-                              color: Colors.green,
-                              size: 60,
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.only(top: 16),
-                              child: Text('Saved'),
-                            )
-                          ];
-                        } else if (snapshot.hasError) {
-                          children = <Widget>[
-                            const Icon(
-                              Icons.error_outline,
-                              color: Colors.red,
-                              size: 60,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 16),
-                              child: Text('Error: ${snapshot.error}'),
-                            )
-                          ];
-                        } else {
-                          children = <Widget>[
-                            const SizedBox(
-                              width: 60,
-                              height: 60,
-                              child: CircularProgressIndicator(),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.only(top: 16),
-                              child: Text('Awaiting result...'),
-                            )
-                          ];
+                        if (item != null) {
+                          items.add(item);
                         }
+                      }
 
-                        return Center(
-                            child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: children));
-                      },
-                    )));
-                  }
-                },
-                child: const Text("Save")
-              )),
+                      for (int i = 0; i < itemCount; i++) {
+                        itemInputFieldList[i].controller.clearTextField();
+                      }
+                    } on ArgumentError catch (e) {
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(e.message)));
+
+                      return;
+                    }
+
+                    if (items.isNotEmpty) {
+                      var itemsInMonth = MapEntry(
+                          MonthTime.fromDateTime(items[0].purchasedTime),
+                          items);
+
+                      UserManager.instance.user.itemlist
+                          .addItemsInMonth(itemsInMonth);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: FutureBuilder<void>(
+                        future: UserManager.instance.saveUser(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<void> snapshot) {
+                          List<Widget> children;
+
+                          if (snapshot.hasData) {
+                            children = <Widget>[
+                              const Icon(
+                                Icons.check_circle_outline,
+                                color: Colors.green,
+                                size: 60,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(top: 16),
+                                child: Text('Saved'),
+                              )
+                            ];
+                          } else if (snapshot.hasError) {
+                            children = <Widget>[
+                              const Icon(
+                                Icons.error_outline,
+                                color: Colors.red,
+                                size: 60,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 16),
+                                child: Text('Error: ${snapshot.error}'),
+                              )
+                            ];
+                          } else {
+                            children = <Widget>[
+                              const SizedBox(
+                                width: 60,
+                                height: 60,
+                                child: CircularProgressIndicator(),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(top: 16),
+                                child: Text('Awaiting result...'),
+                              )
+                            ];
+                          }
+
+                          return Center(
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: children));
+                        },
+                      )));
+                    }
+                  },
+                  child: const Icon(Icons.check))),
         ])));
   }
 }
