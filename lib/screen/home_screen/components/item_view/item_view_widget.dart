e195782327a642aa29f80c875cb79b227d82b394
hoang1007 <hoangvuhuy1007@gmail.com>
2022-07-05 18:06:07 +0700

    make item view more compact

--- lib/screen/home_screen/components/item_view/item_view_widget.dart
+++ lib/screen/home_screen/components/item_view/item_view_widget.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:hah/database/idatabase.dart';
 import 'package:hah/objects/currency.dart';
 import 'package:hah/objects/item.dart';
-import 'package:hah/objects/monthtime.dart';
+import 'package:hah/objects/time.dart';
 import 'package:hah/objects/user.dart';
+import 'package:hah/objects/user_manager.dart';
 import 'package:intl/intl.dart';
 import 'package:month_picker_dialog/month_picker_dialog.dart';
 
 class ItemViewWidget extends StatefulWidget {
-  static const String title = "Purchased Items Viewer";
-
   const ItemViewWidget({Key? key}) : super(key: key);
 
   @override
@@ -44,11 +43,20 @@ class _ItemViewState extends State<ItemViewWidget> {
     });
   }
 
-  List<BottomNavigationBarItem> _createUserBarItems(
-      Iterable<String> usernames) {
+  void _refreshUserIndex(List<User> users) {
+    var newIndex = users.indexOf(UserManager.instance.user);
+
+    if (newIndex != -1 && newIndex != _selectedIndex) {
+      setState(() {
+        _selectedIndex = newIndex;
+      });
+    }
+  }
+
+  List<NavigationDestination> _createUserBarItems(Iterable<String> usernames) {
     return usernames
-        .map((e) => BottomNavigationBarItem(
-            icon: const Icon(Icons.person_outline_rounded), label: e))
+        .map((e) => NavigationDestination(
+            icon: const Icon(Icons.person_outlined), label: e))
         .toList();
   }
 
@@ -73,10 +81,24 @@ class _ItemViewState extends State<ItemViewWidget> {
           var users = snapshot.data!;
           var purchasedInfo = _getPurchasedInfo(users, selectedMonth);
 
+          _refreshUserIndex(users);
+
           return Scaffold(
             appBar: AppBar(
-              title: Text("Item Purchased in $selectedMonth"),
-              centerTitle: true,
+              title: Theme(
+                data: ThemeData.light(),
+                child: DropdownButtonHideUnderline(
+                    child: TextButton(
+                        onPressed: _selectDateTime,
+                        child: Text(selectedMonth.toString()),
+                        style: TextButton.styleFrom(
+                            shadowColor: Colors.blueGrey.shade50,
+                            primary: Colors.white,
+                            textStyle: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)))),
+              ),
+              centerTitle: false,
+              foregroundColor: Colors.white54,
             ),
             body: _ItemListView(
               user: users[_selectedIndex],
@@ -84,22 +106,27 @@ class _ItemViewState extends State<ItemViewWidget> {
               totalPurchased: purchasedInfo["total"]!,
               avgPurchased: purchasedInfo["average"]!,
             ),
-            bottomNavigationBar: BottomNavigationBar(
-              items: _createUserBarItems(users.map((e) => e.name)),
-              currentIndex: _selectedIndex,
-              selectedItemColor: Colors.accents[0],
-              onTap: _onTapped,
-            ),
-            floatingActionButton: ElevatedButton(
-              onPressed: _selectDateTime,
-              child: const Icon(Icons.calendar_month),
-              style: ElevatedButton.styleFrom(
-                shape: const CircleBorder(),
+            bottomNavigationBar: NavigationBarTheme(
+              data: NavigationBarThemeData(
+                  indicatorColor: Colors.blue.shade100,
+                  labelTextStyle: MaterialStateProperty.all(const TextStyle(
+                      fontSize: 14, fontWeight: FontWeight.w500))),
+              child: NavigationBar(
+                destinations: _createUserBarItems(users.map((e) => e.name)),
+                selectedIndex: _selectedIndex,
+                onDestinationSelected: _onTapped,
+                height: 60,
               ),
             ),
           );
         } else {
-          return const CircularProgressIndicator();
+          return const Center(
+            child: SizedBox(
+              height: 60,
+              width: 60,
+              child: CircularProgressIndicator()
+            ),
+          );
         }
       },
     );
@@ -121,14 +148,50 @@ class _ItemListView extends StatelessWidget {
       : super(key: key);
 
   ListView _createItemListView() {
-    return ListView.separated(
-      padding: const EdgeInsets.all(8),
-      itemCount: user.itemlist.getItemsInMonth(month).length,
+    var widgets = _createItemInDayWidgets(user.itemlist.getItemsInMonth(month))
+        .entries
+        .toList();
+
+    return ListView.builder(
       itemBuilder: (context, index) {
-        return _createItemWidget(user.itemlist.getItemsInMonth(month)[index]);
+        return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(widgets[index].key.toString()),
+          widgets[index].value
+        ]);
       },
-      separatorBuilder: (context, index) => const Divider(),
+      itemCount: widgets.length,
+      padding: const EdgeInsets.all(8),
     );
+  }
+
+  Map<DayTime, Widget> _createItemInDayWidgets(List<Item> items) {
+    Map<DayTime, List<Item>> itemsInDay = {};
+
+    for (var item in items) {
+      var day = DayTime.fromDateTime(item.purchasedTime);
+
+      if (itemsInDay.containsKey(day)) {
+        itemsInDay[day]!.add(item);
+      } else {
+        itemsInDay[day] = [item];
+      }
+    }
+
+    return itemsInDay.map((day, items) => MapEntry(
+        day,
+        Material(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(25)),
+            ),
+            color: Colors.lightBlueAccent.withOpacity(0.6),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Wrap(
+                  runSpacing: 10,
+                  children: itemsInDay[day]!
+                      .map((item) => _createItemWidget(item))
+                      .toList()),
+            ))));
   }
 
   Widget _createItemWidget(Item item) {
@@ -138,8 +201,7 @@ class _ItemListView extends StatelessWidget {
         Expanded(
             child: Text(item.price.toString(), textAlign: TextAlign.center)),
         Expanded(
-            child: Text(
-                DateFormat('dd/MM/yyyy hh:mm').format(item.purchasedTime),
+            child: Text(DateFormat('hh:mm').format(item.purchasedTime),
                 textAlign: TextAlign.center)),
       ],
     );
