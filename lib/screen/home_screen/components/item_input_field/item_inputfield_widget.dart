7050557bcc21f9038e5f16f8b7d21e0d9d67b1d4
hoang1007 <hoangvuhuy1007@gmail.com>
2022-07-05 18:05:18 +0700

    change input field to rounded style

--- lib/screen/home_screen/components/item_input_field/item_inputfield_widget.dart
+++ lib/screen/home_screen/components/item_input_field/item_inputfield_widget.dart
@@ -13,8 +13,9 @@ class ItemInputField extends StatefulWidget {
 class _ItemInputFieldState extends State<ItemInputField> {
   TextFormField _createItemNameInputField() {
     return TextFormField(
-      decoration: const InputDecoration(
-          border: UnderlineInputBorder(), hintText: "Item name"),
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(50)),
+          hintText: "Item name"),
       controller: widget.controller.nameInputFieldController,
       autovalidateMode: AutovalidateMode.onUserInteraction,
       validator: widget.controller.nameValidator,
@@ -23,26 +24,28 @@ class _ItemInputFieldState extends State<ItemInputField> {
 
   TextFormField _createItemPriceInputField() {
     return TextFormField(
-      decoration: const InputDecoration(
-        border: UnderlineInputBorder(),
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(50)),
         hintText: "Item price",
-        errorStyle: TextStyle(),
+        errorStyle: const TextStyle(),
       ),
       controller: widget.controller.priceInputFieldController,
       autovalidateMode: AutovalidateMode.onUserInteraction,
       validator: widget.controller.priceValidator,
-      keyboardType: TextInputType.number, // important line, avoid inf loop when user got emoji suggestions
+      keyboardType: TextInputType
+          .number, // important line, avoid inf loop when user got emoji suggestions
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(24),
+        padding: const EdgeInsets.only(left: 24, right: 24, top: 15, bottom: 15),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Expanded(child: _createItemNameInputField()),
+            const SizedBox(width: 10,),
             Expanded(child: _createItemPriceInputField()),
           ],
         ));
